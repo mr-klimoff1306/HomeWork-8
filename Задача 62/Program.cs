0c88c79e29bcc4c209d7774b8c57392dba214454
klimoff <mr.klimoff1306@gmail.com>
2023-02-01 22:10:39 +0300

    Task 62 Solution

---
+++ Задача 62/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+//Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int n = 4;
+
+int[,] arr = new int[n, n];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= n * n)
+{
+    arr[i, j] = num;
+    if (i <= j + 1 && i + j < n - 1)
+        ++j;
+    else if (i < j && i + j >= n - 1)
+        ++i;
+    else if (i >= j && i + j > n - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+
+PrintArray(arr);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
